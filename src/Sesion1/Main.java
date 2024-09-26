13ac2049500e013a25f0fcb71028c2634833ca83
Prieto <Prieto@Prieto>
2024-09-26 18:40:29 +0200

    2-Creacion 2ª rama

--- src/Sesion1/Main.java
+++ src/Sesion1/Main.java
@@ -8,6 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Creacion del proyecto");
+		System.out.println("2-Creacion de la Rama 2");
+		
 	}
 
 }
