16c0e1bfbd4dab3abea029b22f38eb36d6e074dd
Prieto <Prieto@Prieto>
2024-09-26 18:34:35 +0200

    1-Creacion de la rama principal/trunk

---
+++ src/Sesion1/Main.java
@@ -0,0 +1,13 @@
+package Sesion1;
+
+/**
+ * @author Prieto
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creacion del proyecto");
+	}
+
+}
